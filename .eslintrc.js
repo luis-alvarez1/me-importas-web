1b5ec3e777a2d3e084aac73fadb592c5726018ae
Luis Alvarez <lealvarezp@unac.edu.co>
2023-05-28 22:11:51 -0500

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'arrow-body-style': 'off',
+    'no-console': 'off',
+    'implicit-arrow-linebreak': 'off',
+    'no-return-await': 'off',
+    'no-param-reassign': 'off',
+    'no-underscore-dangle': 'off',
+    indent: 'warn',
+    'no-unused-vars': 'warn',
+    'linebreak-style': 'off',
+    'consistent-return': 'off',
+    'function-paren-newline': 'off',
+    'import/prefer-default-export': 'off',
+    'jsx-quotes': 'off',
+    'object-curly-newline': 'off',
+    'react/self-closing-comp': 'warn',
+    'operator-linebreak': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-wrap-multilines': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+  },
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+# me-importas-web
