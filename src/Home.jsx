5d2d3bf2110e3d84afac63b4f955cb57983bbd7d
Luis <lealvarezp@unac.edu.co>
2023-08-16 21:45:52 -0300

    cambios varios, cambio de css a bootstrap en Homem

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import Homem from "./componentes/Homem";
-import Navinci from "./componentes/Navinci";
+import React from 'react';
+import Homem from './componentes/Homem';
+import Navinci from './componentes/Navinci';
 
 function Home() {
-    return (
-        <div>
-            <Navinci />
-            <Homem />
-        </div>
-    )
+  return (
+    <div>
+      <Navinci />
+      <Homem />
+    </div>
+  );
 }
 
 export default Home;
--- src/componentes/Homem.jsx
+++ src/componentes/Homem.jsx
@@ -1,34 +1,39 @@
-import React from "react";
-import "../Styles/Sidebar.css";
+import React from 'react';
+import '../Styles/Sidebar.css';
 
 const Homem = () => {
   return (
     <div>
-    <div className="sidebar">
-  <ul className="menu-list">
-          <h3 className="meimportas">Me importas</h3>
-    <li className="list1">
-      <a href="#" className=" bx bx-building-house">Inicio
-      </a>
-    </li>
-    <li className="list1">
-      <a href="#" className="bx bx-message-rounded-dots">Chat
-      </a>
-    </li>
-    <li className="list1">
-      <a href="#" className="bx bx-group">Grupos
-      </a>
-    </li>
-    <li className="list1">
-      <a href="#" className="bx bxs-heart">Objetivos
-      </a>
-    </li>
-    <li className="list1">
-      <a href="#" className="bx bx-football">Metas
-      </a>
-    </li>
-  </ul>
-    </div>
+      <div className='sidebar'>
+        <ul className='menu-list'>
+          <h3 className='border border-2 border-black text-center p-3'>Me importas</h3>
+          <li className='list1'>
+            <a href='#' className=' bx bx-building-house'>
+              Inicio
+            </a>
+          </li>
+          <li className='list1'>
+            <a href='#' className='bx bx-message-rounded-dots'>
+              Chat
+            </a>
+          </li>
+          <li className='list1'>
+            <a href='#' className='bx bx-group'>
+              Grupos
+            </a>
+          </li>
+          <li className='list1'>
+            <a href='#' className='bx bxs-heart'>
+              Objetivos
+            </a>
+          </li>
+          <li className='list1'>
+            <a href='#' className='bx bx-football'>
+              Metas
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
