735e4a840b2de769bd3923c1ab15972ebba53825
Luis Alvarez <lealvarezp@unac.edu.co>
2023-09-20 20:04:42 -0300

    cambios en estilos

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -54,22 +54,16 @@ const Formulario = () => {
   };
 
   return (
-    <div className="m-0 p-0 bg-primary-subtle" style={{ minHeight: '100vh', width: '100%' }}>
+    <div className="m-0 p-0 bg-primary-subtle d-flex flex-column align-item-center vw-100 vh-100">
       <h1 className="text-center">Inicio de sesion</h1>
       <form
         onSubmit={handleSubmit}
-        className="rounded bg-primary d-flex flex-column text-center p-4 border border-dark border"
         style={{
-          maxWidth: '500px',
-          maxHeight: '1900px',
-          height: '350px',
-          width: '500px',
-          marginLeft: '30%',
-          marginTop: '90px',
-          transform: 'scale(1.3)',
+          alignSelf: 'center',
         }}
+        className="rounded bg-primary d-flex flex-column text-center p-4 border border-dark"
       >
-        <div className="d-flex">
+        <div className="d-flex align-items-center justify-content-center">
           <label htmlFor="" className="mb-20 mx-3">
             <h5>Ingrese el correo</h5>
             <input
@@ -111,8 +105,8 @@ const Formulario = () => {
         <button
           id="button"
           type="submit"
-          className="bg-danger fs-4 fw-bold"
-          style={{ width: '100%', height: '40px', marginTop: '10px' }}
+          className="bg-danger fs-4 fw-bold btn text-white"
+          style={{ marginTop: '10px' }}
         >
           {loading ? 'Cargando...' : 'INICIAR SESIÓN'}
         </button>
